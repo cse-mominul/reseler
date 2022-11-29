6e56c2d49da9fab9a6a1a7420e319f52b939abdb
cse-mominul <cse.mominul.islam>
2022-11-30 05:02:31 +0600

    Update

--- src/Pages/Home/Navbar/Navbar.js
+++ src/Pages/Home/Navbar/Navbar.js
@@ -2,22 +2,37 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../ContextApi/Authcontext';
 
+
 const Navbar = () => {
-    // const { user, logOut } = useContext(AuthContext)
-    // // const userLogOut = () => {
-    // //     logOut()
-    // //         .then(result => {
+    const { user, logOut } = useContext(AuthContext)
+    const userLogOut = () => {
+        logOut()
+            .then(result => {
 
-    // //         })
-    // //         .catch(() => {
+            })
+            .catch(() => {
 
-    // //         })
-    // // }
+            })
+    }
 
-    
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/blog'>Blog</Link></li>
+        {
+            user?.email && <li><Link to='/dashboard'>Dashboard</Link></li>
+        }
 
+        {
+            user?.uid ?
+                <>
+                    <li><button onClick={userLogOut}>Sign Out</button></li>
+                </>
+                :
+                <li><Link to='/Login'>Sign In</Link></li>
+        }
+    </>
     return (
-        
         <div>
             <div className="navbar  bg-black">
                 <div className="navbar-start ">
@@ -26,23 +41,18 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            
+                            {menuItems}
                         </ul>
                     </div>
-                    <Link to='/' className="btn btn-ghost normal-case text-xl text-white">Sale & Sale</Link>
+                    <Link to='/' className="btn btn-ghost normal-case text-xl text-white">Reseler</Link>
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal p-0 text-white">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
-                    <li><Link to='/aboutUs'>About Us</Link></li>
-                    <li><Link to='/Login'>Sign In</Link></li>
+                        {menuItems}
                     </ul>
                 </div>
             </div>
         </div>
-            
-      
     );
 };
 
