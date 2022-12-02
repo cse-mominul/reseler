abcd8397cbc8238a4931fe6f1557ff1a232f1d76
cse-mominul <cse.mominul.islam>
2022-12-02 19:45:22 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Authcontext from './ContextApi/Authcontext';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <QueryClientProvider client={queryClient}>
    <Authcontext>
     <App></App>
    </Authcontext>
-  
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
