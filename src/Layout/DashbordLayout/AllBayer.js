c844648b87e9d06d5dcf36b736f22d22c7920fad
cse-mominul <cse.mominul.islam>
2022-12-02 19:52:19 +0600

    updated

--- src/Layout/DashbordLayout/AllBayer.js
+++ src/Layout/DashbordLayout/AllBayer.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../ContextApi/Authcontext';
+
 
 const AllBayer = () => {
+    const { user } = useContext(AuthContext)
+    const { data: buyers = [], isLoading } = useQuery({
+        queryKey: ["bayer", user?.email],
+        queryFn: async () => {
+            const res = await fetch(`https://used-product-laptop-market-server.vercel.app/bayer?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('Access-token')}`
+                }
+            });
+            const data = await res.json();
+            return data;
+        },
+    });
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
     return (
-        <div>
-            
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>serial</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        buyers?.length &&
+                        buyers.map((bayer, index) => (
+                            <tr key={bayer._id}>
+                                <th>{index + 1}</th>
+                                <th> {bayer.name} </th>
+                                <th>{bayer.email}</th>
+                                <th><button className="btn btn-sm bg-accent">Delete</button></th>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
         </div>
-    );
+    )
 };
 
 export default AllBayer;
+
+
--- src/Layout/DashbordLayout/MyProduct.js
+++ src/Layout/DashbordLayout/MyProduct.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../ContextApi/Authcontext';
+
 
 const MyProduct = () => {
+    const { user } = useContext(AuthContext)
+
+    const { data: myProduct, isLoading } = useQuery({
+        queryKey: ['myProduct', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`https://used-product-laptop-market-server.vercel.app/myProduct?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('Access-token')}`
+                }
+            })
+            const data = await res.json()
+            return data;
+        }
+
+    })
+    if (isLoading) {
+        return <h1 className='text-3xl'>Loading...</h1>
+    }
     return (
         <div>
-            
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>product Name</th>
+                            <th>Price </th>
+                            <th> Status </th>
+                            <th> Advertise </th>
+                            <th> Delete </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            myProduct?.length &&
+                            myProduct.map((product) => (
+                                <tr key={product._id}>
+                                    <th></th>
+                                    <th> {product.product_name} </th>
+                                    <th> {product.product_price} </th>
+                                    <th>
+                                        {" "}
+                                        <button className="btn btn-sm btn-accent">
+                                            available
+                                        </button>{" "}
+                                    </th>
+                                    <th>
+                                        {" "}
+
+                                    </th>
+                                    <th>
+                                        {" "}
+                                        <button className="btn btn-sm btn-outline">
+                                            Delete
+                                        </button>{" "}
+                                    </th>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
-
 export default MyProduct;
